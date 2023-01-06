511874a2b507a0119e76b7eb415179263bf4628d
ahmed141631 <92031163+ahmed141631@users.noreply.github.com>
2023-01-06 22:35:22 +0200

    done

--- src/Task.h
+++ src/Task.h
@@ -1,5 +1,9 @@
 #ifndef TASK_H
 #define TASK_H
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
 
 #pragma once
 
@@ -8,9 +12,18 @@ class Task
 public:
     Task();
     ~Task();
+    
 
-private:
 
+protected:
+    string TaskName ;
+    string Taskcontent;
+    string TaskStatus; // task status string for telling the state whether completed or not
+    /* and then suppose to show datetime and rest of the time */
+    time_t StartTime;
+    time_t EndTime;
+    time_t leftTime;
+    
 };
 
 #endif
